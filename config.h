89a6b5764e9eb4842612f888d9db61d3314a3199
rahulsenna <rahulvsenna@gmail.com>
2022-07-11 10:51:13 +0000

    updated

--- config.h
+++ config.h
@@ -19,16 +19,17 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "",  "", "" }; 
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class              instance    title       tags mask     isfloating   monitor */
+	{ "Sublime_text",     NULL,       NULL,       1,            0,           -1 },
+	{ "Google-chrome",    NULL,       NULL,       2,            0,           -1 },
 };
 
 /* layout(s) */
