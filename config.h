227b9e5d350aac4593659608946929453ff851a6
rahulsenna <rahulvsenna@gmail.com>
2022-07-12 02:54:36 +0000

    added audio keys

--- config.h
+++ config.h
@@ -1,5 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
+/* Audio */
+#include <X11/XF86keysym.h>
+
+/* Add somewhere in your constants definition section */
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+
+/* If you use amixer, use this instead. Thanks go to DaniOrt3ga. */
+// static const char *upvol[] = { "/usr/bin/amixer", "set", "Master", "5%+", NULL };
+// static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-", NULL };
+// static const char *mutevol[] = { "/usr/bin/amixerl", "set", "Master", "toggle", NULL };
+
+/* To use light add this to the constant definition section. Thanks Hritik14. */
+static const char *light_up[] = {"/usr/bin/light", "-A", "5", NULL};
+static const char *light_down[] = {"/usr/bin/light", "-U", "5", NULL};
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
@@ -61,9 +78,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *sublime[]  = { "subl", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_1,      spawn,          {.v = sublime } },
 	{ MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -100,6 +119,13 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+
+	{ 0,                            XF86XK_AudioLowerVolume,     spawn, {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute, 			 spawn, {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume,     spawn, {.v = upvol   } },
+	{ 0,					        XF86XK_MonBrightnessUp,		 spawn,	{.v = light_up} },
+	{ 0,					        XF86XK_MonBrightnessDown,	 spawn,	{.v = light_down} },
 };
 
 /* button definitions */
