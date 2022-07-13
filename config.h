9d78c3901fe29d791a5efc69dd4e7cc40abeab46
Rahul Senna <rahulvsenna@gmail.com>
2022-07-13 23:26:19 +0530

    update

--- config.h
+++ config.h
@@ -79,10 +79,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *sublime[]  = { "subl", NULL };
+static const char *chrome[]  =  { "google-chrome-stable", NULL };
+static const char *xrandr[]  =  { "xrandr", "-r", "60", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_1,      spawn,          {.v = sublime } },
+	{ MODKEY,                       XK_grave,  spawn,          {.v = chrome } },
+	{ MODKEY,                       XK_grave,  spawn,          {.v = xrandr } },
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
