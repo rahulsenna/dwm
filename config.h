65bf6e9ac018a0d56623d756fcc46cfeed30dfac
Rahul Senna <rahulvsenna@gmail.com>
2022-07-24 19:18:34 +0530

    updated

--- config.h
+++ config.h
@@ -23,7 +23,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrains Mono Medium:size=13" };
 static const char dmenufont[]       = "monospace:size=20";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -45,8 +45,8 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "", "", "", "", "",  "", "" }; 
-
+// static const char *tags[] = { "", "", "", "", "", "", "",  "", "" }; 
+static const char *tags[] = { "📖", "🌎", "🐛", "🖌","ext" }; 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
