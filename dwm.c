b1d9d1249a0423eab4bd247b16090513d74fb82c
Rahul Senna <rahulvsenna@gmail.com>
2023-03-21 15:15:39 +0530

    changed shiftview behaviour so it doesn't spawn scratchpads even if they are switched off

--- dwm.c
+++ dwm.c
@@ -2321,15 +2321,14 @@ shiftview(const Arg *arg) {
 	Arg shifted;
 
 	if(arg->i > 0) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i);
 
 	else // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i);
 
 	view(&shifted);
 }
+
 
 int
 main(int argc, char *argv[])
